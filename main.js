6f831b8e018d8323c97a30217af324934b86caf9
Nashua32 <56478217+Nashua32@users.noreply.github.com>
2021-01-24 12:02:06 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+let maidatum = String((new Date()).getFullYear())+ String((new Date()).getMonth()) + String((new Date()).getDate())
+
+/*if (localStorage.datum) {
+    if (maidatum != localStorage.datum) {
+        localStorage.datum = maidatum
+        localStorage.napiszam  = Math.floor(Math.random() * kerdesek.length)
+    }
+    if (! localStorage.napiszam) {
+        localStorage.napiszam = 0
+    }
+}
+else {
+    localStorage.datum =  String((new Date()).getFullYear())+ String((new Date()).getMonth()) + String((new Date()).getDate())
+}
+
+document.write(kerdesek[parseInt(localStorage.napiszam)])
+
+function ujNapi() {
+    localStorage.napiszam  = Math.floor(Math.random() * kerdesek.length)
+    window.location.reload()
+}
+
+function showAnswer() {
+    document.getElementById("valasz").innerHTML = valaszok[parseInt(localStorage.napiszam)]
+    document.getElementById("eltalaltad?").innerHTML = "Helyes választ adtál?"
+    jovolt.style = "display:inline-block"
+    nemvoltjo.style = "display:inline-block"
+}*/
